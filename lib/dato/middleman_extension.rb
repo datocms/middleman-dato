3f665e7e83280f0e615dcdde27d430912f87ec9f
Stefano Verna <s.verna@cantierecreativo.net>
2015-05-18 18:52:38 +0200

    Better sync

--- lib/dato/middleman_extension.rb
+++ lib/dato/middleman_extension.rb
@@ -14,10 +14,23 @@ module Dato
       super
 
       Repo.instance.connection_options = options
+      Repo.instance.sync!
 
       app.before do
-        Repo.instance.sync!
+        unless build?
+          print "Syncing Dato space... "
+          Repo.instance.sync!
+          puts "done."
+        end
         true
+      end
+
+      app.send :include, InstanceMethods
+    end
+
+    module InstanceMethods
+      def dato
+        OpenStruct.new(Repo.instance.records_per_content_type)
       end
     end
 
